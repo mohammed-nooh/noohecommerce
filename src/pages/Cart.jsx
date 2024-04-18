82cd1a26c8ed17027ab1cc00a84007c531dcdd9b
Mohammed Nooh <83815358+mohammed-nooh@users.noreply.github.com>
2024-04-18 18:58:17 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,12 +18,16 @@ const Top=styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding:20px;
 `
 
 const TopButton=styled.button`
     padding: 10px;
     font-weight: 600;
     cursor: pointer;
+    border: ${props=>props.type === "filled" && "none"};
+    background-color: ${props=>props.type === "filled" ? "black" : "transparent"};
+    color: ${props=>props.type === "filled" && "white"};
 `
 const TopTexts=styled.div`
     
@@ -35,7 +39,48 @@ const TopText=styled.span`
     cursor: pointer;
 `
 
-const Bottom=styled.div``
+const Bottom=styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Info=styled.div`
+    flex: 3;
+`
+const Product=styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductDetaill=styled.div`
+    flex: 2;
+    display: flex;
+`
+const Image=styled.img`
+    width: 200px;
+`
+const Details=styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName=styled.span``
+const ProductId=styled.span``
+const ProductColor=styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: 1px solid black;
+    background-color: ${(props)=>props.color};
+`
+const ProductSize=styled.span``
+const PriceDetails=styled.span`
+    flex: 1;
+`
+
+const Summary=styled.div`
+    flex: 1;
+`
 
 const Cart = () => {
   return (
@@ -50,9 +95,27 @@ const Cart = () => {
                     <TopText>Shopping Bag(2)</TopText>
                     <TopText>Your Wishlist (0)</TopText>
                 </TopTexts>
-                <TopButton>CHECKOUT NOW</TopButton>
+                <TopButton type="filled">CHECKOUT NOW</TopButton>
             </Top>
-            <Bottom></Bottom>
+            <Bottom>
+                <Info>
+                    <Product>
+                        <ProductDetaill>
+                            <Image src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSPrigyutrkRxSqxdzKGKwnT9V_Vv71uIXQadDEg0uGNOKe1mCfITZD_2gQnXPUIbeXU3VIJa3ueh9WkWrvARHSBaFshNa5JfNVoSRsulY_oGwIyCXYq7URIg"/>
+                            <Details>
+                                <ProductName><b>PRODUCT: </b> JESSIE THUNDER SHOES</ProductName>
+                                <ProductId><b>ID: </b>234876</ProductId>
+                                <ProductColor color="lightgrey"/>
+                                <ProductSize><b>Size: </b>37.5</ProductSize>
+                            </Details>
+                        </ProductDetaill>
+                        <PriceDetails>
+                            Price
+                        </PriceDetails>
+                    </Product>
+                </Info>
+                <Summary>summary</Summary>
+            </Bottom>
         </Wrapper>
         <Footer/>
     </Container>
